7d227efd63cc73a8a183c51550128a517ed1b90f
Alexey Borisov <borisovalexey1996@gmail.com>
2024-04-06 22:53:39 +0400

    Comments on Magma.h

--- research-work/Magma.h
+++ research-work/Magma.h
@@ -1,16 +1,16 @@
 #pragma once
 #include <stdint.h>
 #include <inttypes.h>
-#include <algorithm>
+#include <algorithm>      // In a header file only the headers necessary to DECLARE the structures and functions must be present
 #include <vector>
 #include <immintrin.h>
 #include <sstream>
 #include <span>
-
+ 
 struct key {
 	uint8_t bytes[32];
-	explicit key(uint8_t* data) {
-		std::copy_n(data, 32, bytes);
+	explicit key(uint8_t* data) {              // 1. Why the function body is placed in the header file? 
+		std::copy_n(data, 32, bytes);      // 2. And for this simple action you included the entire <algorithm>? 
 	}
 };
 
@@ -18,7 +18,7 @@ union halfVectorMagma {
 	uint8_t bytes[sizeof(uint32_t)];
 	uint32_t vector;
 	halfVectorMagma() = default;
-	halfVectorMagma(const uint32_t src) : vector{ src } {}
+	halfVectorMagma(const uint32_t src) : vector{ src } {} //  Why the function body is placed in header file? 
 };
 
 union byteVectorMagma {
@@ -29,9 +29,9 @@ union byteVectorMagma {
 	halfVectorMagma halfs[2];
 	uint8_t bytes[8];
 	byteVectorMagma() = default;
-	byteVectorMagma(const halfVectorMagma& left, const halfVectorMagma& right) : lo{ left }, hi{ right } {}
-	explicit byteVectorMagma(uint8_t* data) {
-		std::copy_n(data, 8, bytes);
+	byteVectorMagma(const halfVectorMagma& left, const halfVectorMagma& right) : lo{ left }, hi{ right } {} //  Why the function body is placed in header file? 
+	explicit byteVectorMagma(uint8_t* data) {  // 1.Why the function body is placed in header file? 
+		std::copy_n(data, 8, bytes);       // 2.And for this simple action you included the entire <algorithm>? 
 	}
 };
 
@@ -39,5 +39,5 @@ class Magma {
 public:
 	Magma(const key& key);
 	void encryptTextAVX(const std::vector<byteVectorMagma>& data, std::vector<byteVectorMagma>& dest) const;
-	void encryptTextAVX2(std::span<byteVectorMagma> src, std::span<byteVectorMagma> dest)  const; 
+	void encryptTextAVX2(std::span<byteVectorMagma> src, std::span<byteVectorMagma> dest)  const;   // better define src  as std::span<const byteVectorMagma>
 };
