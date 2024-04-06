6f0f454cdfb4263f81f6d75f08addd1f78468647
funSuicide <90608736+funSuicide@users.noreply.github.com>
2024-04-06 19:08:02 +0400

    добавлен скрипт генерации таблиц

---
+++ research-work/main.py
@@ -0,0 +1,79 @@
+'''
+
+START_TABLE = [[1, 7, 14, 13, 0, 5, 8, 3, 4, 15, 10, 6, 9, 12, 11, 2],
+               [1, 7, 14, 13, 0, 5, 8, 3, 4, 15, 10, 6, 9, 12, 11, 2],
+               [7, 15, 5, 10, 8, 1, 6, 13, 0, 9, 3, 14, 11, 4, 2, 12],
+               [12, 8, 2, 1, 13, 4, 15, 6, 7, 0, 10, 5, 3, 14, 9, 11],
+               [11, 3, 5, 8, 2, 15, 10, 13, 14, 1, 7, 4, 12, 9, 6, 0],
+               [11, 3, 5, 8, 2, 15, 10, 13, 14, 1, 7, 4, 12, 9, 6, 0],
+               [6, 8, 2, 3, 9, 10, 5, 12, 1, 14, 4, 7, 11, 13, 0, 15],
+               [12, 4, 6, 2, 10, 5, 11, 9, 14, 8, 13, 7, 0, 3, 15, 1]]
+
+'''
+START_TABLE = [[12, 4, 6, 2, 10, 5, 11, 9, 14, 8, 13, 7, 0, 3, 15, 1],
+               [6, 8, 2, 3, 9, 10, 5, 12, 1, 14, 4, 7, 11, 13, 0, 15],
+               [11, 3, 5, 8, 2, 15, 10, 13, 14, 1, 7, 4, 12, 9, 6, 0],
+               [11, 3, 5, 8, 2, 15, 10, 13, 14, 1, 7, 4, 12, 9, 6, 0],
+               [12, 8, 2, 1, 13, 4, 15, 6, 7, 0, 10, 5, 3, 14, 9, 11],
+               [7, 15, 5, 10, 8, 1, 6, 13, 0, 9, 3, 14, 11, 4, 2, 12],
+               [1, 7, 14, 13, 0, 5, 8, 3, 4, 15, 10, 6, 9, 12, 11, 2],
+               [1, 7, 14, 13, 0, 5, 8, 3, 4, 15, 10, 6, 9, 12, 11, 2]]
+
+
+def get_table_4x256():
+    result = [[], [], [], []]
+    for i in range(4):
+        for j in range(256):
+            result[i].append(START_TABLE[2 * i][j & 0xf])
+            result[i][j] = result[i][j] << 4
+            result[i][j] = result[i][j] | START_TABLE[2 * i + 1][j >> 4]
+    return result
+
+
+def get_table_2x65536(table):
+    result = [[], []]
+    for i in range(2):
+        for j in range(65536):
+            if i == 1:
+                a = 5
+            result[i].append(table[2 * i][j & 0xff])
+            result[i][j] = result[i][j] << 8
+            result[i][j] = result[i][j] | table[2 * i + 1][j >> 8]
+    return result
+
+
+def create_file(table, filename, signature):
+    with open(filename, 'w') as f:
+        f.write('#pragma once\n\n')
+        f.write(signature + ' = {\n')
+        for i in range(len(table)):
+            f.write('\t\t\t\t{ ')
+            for j in range(len(table[0])):
+                f.write(str(table[i][j]))
+                if j != len(table[0]) - 1:
+                    f.write(', ')
+            f.write(' },\n')
+        f.write('};')
+
+
+def check(table):
+    for i in range(len(table)):
+        tmp = set(table[i])
+        if len(tmp) != len(table[i]):
+            return False
+    return True
+
+
+def main():
+    table_4x256 = get_table_4x256()
+    print(check(table_4x256))
+    table_2x65536 = get_table_2x65536(table_4x256)
+    print(check(table_2x65536))
+    create_file(table=table_4x256, filename='table4X256.hpp', signature='static constexpr uint8_t sTable4x256[4][256]')
+    create_file(table=table_2x65536, filename='table2X65536.hpp',
+                signature='static constexpr uint16_t sTable2x65536[2][1<<16]')
+    return 0
+
+
+if __name__ == '__main__':
+    main()
