5c3af865c730e721f319a426d0acedc493059abd
funSuicide <Digital-Corsair@yandex.com>
2024-06-04 19:40:42 +0400

    added new calss for Kuznechik AVX512 and new executable file

---
+++ research-work/KuznechikAVX512.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <immintrin.h>
+#include <span>
+#include "Structures.h"
+
+class KuznechikAVX512 {
+private:
+    byteVectorKuznechik roundKeysKuznechik[10][2];
+public:
+    KuznechikAVX512(const key& key);
+    void processDataGamma(std::span<const byteVectorKuznechik> src, std::span<byteVectorKuznechik> dest, uint64_t iV) const;
+    void processData(std::span<const byteVectorKuznechik> src, std::span<byteVectorKuznechik> dest, bool en)  const;
+};
+
--- research-work/research-work.cpp
+++ research-work/research-work.cpp
@@ -16,229 +16,8 @@
 #include <filesystem>
 #define GIGABYTE 1024*1024*1024
 
-using duration_t = std::chrono::duration<float>;
-
-template<typename typeVector>
-std::string printVector (const typeVector& block) {
-	std::string result;
-	for (int i = sizeof(typeVector) - 1; i >= 0; --i) {
-		uint8_t high, low;
-		high = block.bytes[i] >> 4;
-		low = block.bytes[i] & 0xf;
-		result.push_back((char)(high < 10 ? ('0' + high) : ('A' + high - 10)));
-		result.push_back((char)(low < 10 ? ('0' + low) : ('A' + low - 10)));
-	}
-	return result;
-}
-
-template<typename typeVector>
-void generateTestVector(std::vector<typeVector>& dest)
-{
-	dest.resize(GIGABYTE / sizeof(typeVector));
-	for (size_t i = 0; i < dest.size(); ++i)
-	{
-		dest[i].halfsData.lo = 17721245453 * i;
-		dest[i].halfsData.hi = 17721245453 * i + 13243342;
-	}
-}
-
-void magmaTestSpeed()
-{
-	byteVectorMagma testBlockMagma(testDataBytesMagma);
-	key testKeyMagma(testKeyBytesMagma);
-
-	MagmaAVX2 MAVX2(testKeyMagma);
-
-	byteVectorMagma expectedBlockMagma(expectedBlockBytesMagma);
-
-	std::vector<byteVectorMagma> testSrcMagma = { testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma };
-	std::vector<byteVectorMagma> testDestMagma(8);
-
-	std::vector<byteVectorMagma> testSrcMagma512 = { testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma };
-	std::vector<byteVectorMagma> testDestMagma512(16);
-
-
-	std::cout << "Тестовые данные: " << printVector(testBlockMagma) << std::endl;
-	std::cout << "Тестовый ключ: " << printVector(testKeyMagma) << std::endl;
-	std::cout << "Ожидаемый результат: " << printVector(expectedBlockMagma) << std::endl;
-
-	MAVX2.processData(testSrcMagma, testDestMagma, 1);
-
-	std::cout << "Реальный результат: " << printVector(testDestMagma[0]) << std::endl;
-
-	std::cout << "----------------------------------------------" << std::endl;
-
-	std::cout << "Запуск теста скорости..." << std::endl;
-	
-	std::vector<float> times2;
-	std::vector<byteVectorMagma> vectorForMagma;
-	generateTestVector(vectorForMagma);
-
-	uint8_t c[32] = "asdafasdasdasfdasdasdakfksakfsa";
-	key S(c);
-	MagmaAVX2 M(S);
-
-	std::span<byteVectorMagma, GIGABYTE / 8> tmpMagma(vectorForMagma);
-
-	for (int j = 0; j < 5; ++j) {
-		std::vector<byteVectorMagma> b(GIGABYTE / 8);
-		std::span<byteVectorMagma, GIGABYTE / 8> dest(b);
-		auto begin = std::chrono::steady_clock::now();
-		M.processData(tmpMagma, dest, 1);
-		auto end = std::chrono::steady_clock::now();
-		auto time = std::chrono::duration_cast<duration_t>(end - begin);
-		times2.push_back(time.count());
-		std::cout << "Обработано: " << j+1 << " ГБ" << ": " << time.count() << std::endl;
-	}
-
-	double meanM2 = std::accumulate(times2.begin(), times2.end(), 0.0) / times2.size();
-	std::cout << meanM2 << std::endl;
-	std::cout << "Среднее значение скорости алгоритма (AVX-2): " << 1 / meanM2 << "ГБ/с" << std::endl;
-
-	std::cout << "----------------------------------------------" << std::endl;
-	
-}
-
-
-void kuznechikTestSpeed() 
-{
-	byteVectorKuznechik testBlockKuz(testDataBytesKuz);
-
-	byteVectorKuznechik expectedBlockKuz(expectedBlockBytesKuz);
-	key testKeyKuz(testKeyBytesKuz);
-
-	std::vector<byteVectorKuznechik> testSrcKuz = { testBlockKuz, testBlockKuz, testBlockKuz, testBlockKuz };
-	std::vector<byteVectorKuznechik> testDestKuz(4);
-
-	Kuznechik KAVX2(testKeyKuz);
-
-	std::cout << "Тестовые данные: " << printVector(testBlockKuz) << std::endl;
-	std::cout << "Тестовый ключ: " << printVector(testKeyKuz) << std::endl;
-	std::cout << "Ожидаемый результат: " << printVector(expectedBlockKuz) << std::endl;
-
-	KAVX2.processData(testSrcKuz, testDestKuz, 1);
-
-	std::cout << "Реальный результат: " << printVector(testDestKuz[0]) << std::endl;
-
-	std::cout << "----------------------------------------------" << std::endl;
-
-	std::cout << "Запуск теста скорости..." << std::endl;
-
-	std::vector<float> timesKuz2;
-
-	std::vector<byteVectorKuznechik> a;
-	generateTestVector(a);
-
-	uint8_t c[32] = "asdafasdasdasfdasdasdakfksakfsa";
-	key S(c);
-	Kuznechik k2(S);
-
-	std::span<byteVectorKuznechik, GIGABYTE / 16> aa2(a);
-	for (int j = 0; j < 5; ++j) {
-		std::vector<byteVectorKuznechik> b(GIGABYTE / 16);
-		std::span<byteVectorKuznechik, GIGABYTE / 16> dest(b);
-		auto begin = std::chrono::steady_clock::now();
-		k2.processData(aa2, dest, 1);
-		auto end = std::chrono::steady_clock::now();
-		auto time = std::chrono::duration_cast<duration_t>(end - begin);
-		timesKuz2.push_back(time.count());
-		std::cout << "Обработано: " << j+1 << " ГБ" << ": " << time.count() << std::endl;
-	}
-
-	double meanKuz2 = std::accumulate(timesKuz2.begin(), timesKuz2.end(), 0.0) / timesKuz2.size();
-	std::cout << meanKuz2 << std::endl;
-	std::cout << "Среднее значение скорости алгоритма (AVX-2): " << 1 / meanKuz2 << "ГБ/с" << std::endl;
-
-	std::cout << "----------------------------------------------" << std::endl;
-}
-
-void kuznechik512TestSpeed()
-{
-	std::vector<float> timesKuz2;
-
-	std::vector<byteVectorKuznechik> a;
-	generateTestVector(a);
-
-	uint8_t c[32] = "asdafasdasdasfdasdasdakfksakfsa";
-	key S(c);
-	Kuznechik k2(S);
-
-	std::span<byteVectorKuznechik, GIGABYTE / 16> aa2(a);
-	for (int j = 0; j < 5; ++j) {
-		std::vector<byteVectorKuznechik> b(GIGABYTE / 16);
-		std::span<byteVectorKuznechik, GIGABYTE / 16> dest(b);
-		auto begin = std::chrono::steady_clock::now();
-		k2.processDataAVX512(aa2, dest, 1);
-		auto end = std::chrono::steady_clock::now();
-		auto time = std::chrono::duration_cast<duration_t>(end - begin);
-		timesKuz2.push_back(time.count());
-		std::cout << "Обработано: " << j+1 << " ГБ" << ": " << time.count() << std::endl;
-	}
-
-	double meanKuz2 = std::accumulate(timesKuz2.begin(), timesKuz2.end(), 0.0) / timesKuz2.size();
-	std::cout << meanKuz2 << std::endl;
-	std::cout << "Среднее значение скорости алгоритма (AVX-512): " << 1 / meanKuz2 << "ГБ/с" << std::endl;
-
-	std::cout << "----------------------------------------------" << std::endl;
-}
-
-void magma512TestSpeed(bool reg)
-{
-	byteVectorMagma testBlockMagma(testDataBytesMagma);
-	key testKeyMagma(testKeyBytesMagma);
-
-	MagmaAVX512 MAVX512(testKeyMagma);
-
-	byteVectorMagma expectedBlockMagma(expectedBlockBytesMagma);
-
-	std::vector<byteVectorMagma> testSrcMagma512 = { testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma, testBlockMagma };
-	std::vector<byteVectorMagma> testDestMagma512(16);
-
-
-	std::cout << "Тестовые данные: " << printVector(testBlockMagma) << std::endl;
-	std::cout << "Тестовый ключ: " << printVector(testKeyMagma) << std::endl;
-	std::cout << "Ожидаемый результат: " << printVector(expectedBlockMagma) << std::endl;
-
-	MAVX512.processData(testSrcMagma512, testDestMagma512, 1, reg);
-
-	std::cout << "Реальный результат: " << printVector(testDestMagma512[0]) << std::endl;
-
-	std::cout << "----------------------------------------------" << std::endl;
-
-	std::cout << "Запуск теста скорости..." << std::endl;
-	
-	std::vector<float> times2;
-	std::vector<byteVectorMagma> vectorForMagma;
-	generateTestVector(vectorForMagma);
-
-	uint8_t c[32] = "asdafasdasdasfdasdasdakfksakfsa";
-	key S(c);
-	MagmaAVX512 M(S);
-
-	std::span<byteVectorMagma, GIGABYTE / 8> tmpMagma(vectorForMagma);
-
-	for (int j = 0; j < 5; ++j) {
-		std::vector<byteVectorMagma> b(GIGABYTE / 8);
-		std::span<byteVectorMagma, GIGABYTE / 8> dest(b);
-		auto begin = std::chrono::steady_clock::now();
-		M.processData(tmpMagma, dest, 1, reg);
-		auto end = std::chrono::steady_clock::now();
-		auto time = std::chrono::duration_cast<duration_t>(end - begin);
-		times2.push_back(time.count());
-		std::cout << "Обработано: " << j+1 << " ГБ" << ": " << time.count() << std::endl;
-	}
-
-	double meanM2 = std::accumulate(times2.begin(), times2.end(), 0.0) / times2.size();
-	std::cout << meanM2 << std::endl;
-	std::cout << "Среднее значение скорости алгоритма (AVX-512): " << 1 / meanM2 << "ГБ/с" << std::endl;
-
-	std::cout << "----------------------------------------------" << std::endl;
-	
-}
-
 bool checkFile(const std::string& path)
 {
-	// std::filesystem::exists
 	return std::filesystem::exists(path);
 }
 
@@ -251,7 +30,6 @@ int main(int argc, char* argv[]) {
 	enum Mode {
 		ENCRYPT,
 		DECRYPT,
-		TEST
 	};
 
 	enum Algorithm {
@@ -380,11 +158,6 @@ int main(int argc, char* argv[]) {
 			auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
 			std::cout << "Общее время дешифрования: " << time.count() << " ms" << std::endl;
 		}
-		else if (mode == 2)
-		{
-			magmaTestSpeed();
-		}
-		break;
 	}
 	case 2:
 	{
@@ -404,10 +177,6 @@ int main(int argc, char* argv[]) {
 			auto end = std::chrono::steady_clock::now();
 			auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
 			std::cout << "Общее время дешифрования: " << time.count() << " ms" << std::endl;
-		}
-		else if (mode == 2)
-		{
-			kuznechikTestSpeed();
 		}
 		break;
 	}
@@ -429,10 +198,6 @@ int main(int argc, char* argv[]) {
 			auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
 			std::cout << "Общее время дешифрования: " << time.count() << " ms" << std::endl;
 		}
-		else if (mode == 2)
-		{
-			kuznechik512TestSpeed();
-		}
 		break;
 	}
 	case 4:
@@ -453,10 +218,6 @@ int main(int argc, char* argv[]) {
 			auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
 			std::cout << "Общее время дешифрования: " << time.count() << " ms" << std::endl;
 		}
-		else if (mode == 2)
-		{
-			magma512TestSpeed(0);
-		}
 		break;
 	}
 	case 5:
@@ -476,10 +237,6 @@ if (mode == 1)
 			auto end = std::chrono::steady_clock::now();
 			auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
 			std::cout << "Общее время дешифрования: " << time.count() << " ms" << std::endl;
-		}
-		else if (mode == 2)
-		{
-			magma512TestSpeed(1);
 		}
 		break;
 	}
